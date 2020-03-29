4e665504b388643124c67bf2ce5bdf5787d61913
rogue67 <roger.forsman67@gmail.com>
2020-03-29 17:31:41 +0200

    temporary file for improved code

--- pos_index.m
+++ pos_index.m
@@ -1,6 +1,6 @@
 
 function posIndex = pos_index(V,s)
-%POS_INDEX Version 1 av Funktion för att hitte det index för det första värdet
+%POS_INDEX Version 2 av Funktion för att hitte det index för det första värdet
 %i den sorterade vektorn V där V[index] >= talet s
 %
 %
@@ -9,23 +9,7 @@ function posIndex = pos_index(V,s)
 ti = 0; %temporärt index
 L = length(V);
 
-for k = 1:L
-    if (V(k) < s)
-        ti = k+1;
-    elseif (V(k) == s)
-        ti = k;
-    end
-end
 
-t = V(L);
-l1 = (ti > L) && ( t ~= s ); 
-l2 = ( ti == 0);
-
-if l1 || l2
-    "Talet finns ej i vektorn!"
-else
-    "Index för det tal som är större eller lika med s:"
-end
 
 posIndex = ti;
 end
